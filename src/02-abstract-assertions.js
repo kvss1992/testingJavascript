8637bafeb492eb1644413ee5f7b31970e94c3046
Karanvir Sawhney <kvss1992@users.noreply.github.com>
2019-02-10 01:13:18 -0500

    Added maths.js

--- src/02-abstract-assertions.js
+++ src/02-abstract-assertions.js
@@ -1,13 +1,11 @@
 const sum = (a, b) => a + b;
+const sub = (a, b) => a - b;
 
 let result = sum(3, 9);
-let expected = 12;
+let expected = 10;
 
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`);
-}
+expect(result).toBe(expected);
 
-const sub = (a, b) => a - b;
 result = sub(6, 2);
 expected = 4;
 if (result !== expected) {
