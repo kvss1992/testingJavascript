1e451ddfa40eef6c56869d4f31fbab6cf25dcc90
Karanvir Sawhney <kvss1992@users.noreply.github.com>
2019-02-10 01:09:56 -0500

    throw-an-error

--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
-// var http = require("http");
-
-// //create a server object:
-// http
-//   .createServer(function(req, res) {
-//     res.write("Hello World!"); //write a response to the client
-//     res.end(); //end the response
-//   })
-//   .listen(8080); //the server object listens on port 8080
-
-// const sum = (a, b) => a - b;
 const sum = (a, b) => a + b;
-const subtract = (a, b) => a - b;
+const sub = (a, b) => a - b;
 
-let result = sum(3, 7);
+let result = sum(3, 9);
 let expected = 12;
 
 if (result !== expected) {
   throw new Error(`${result} is not equal to ${expected}`);
 }
 
-result = subtract(7, 4);
-expected = 3;
-
+result = sub(6, 2);
+expected = 4;
 if (result !== expected) {
   throw new Error(`${result} is not equal to ${expected}`);
 }
